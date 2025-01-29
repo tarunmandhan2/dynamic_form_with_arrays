d77ae2d14b0c384c91af67b955eaecaee3524bad
Tarun2 <141573811+tarunmandhan2@users.noreply.github.com>
2025-01-29 13:51:50 +0000

    done Add,remove functionally and disable button working

--- src/DynamicForm.jsx
+++ src/DynamicForm.jsx
@@ -1,7 +1,71 @@
+import { useEffect, useState } from "react";
+
 const DynamicForm = () => {
+  const [feildList, setFeildList] = useState([{ val: "", id: Date.now() }]);
+  const [isDisable, setIsDisable] = useState(true);
+
+  const addFeild = () => {
+    // console.log("Add Feild");
+    setFeildList([...feildList, { val: "", id: Date.now() }]);
+  };
+
+  const handleRemove = (i) => {
+    const newFeildList = [...feildList];
+    newFeildList.splice(i, 1);
+    setFeildList(newFeildList);
+
+    // console.log("🚀", newFeildList, "-----", i);
+  };
+
+  const handleChange = (i, e) => {
+    // console.log("inside handle Change", feildList[i]);
+
+    const newFieldList = [...feildList];
+    newFieldList[i] = { ...newFieldList[i], val: e.target.value };
+    setFeildList([...newFieldList]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submited 🚀 ", feildList);
+  };
+
+  useEffect(() => {
+    if (feildList.every((obj) => obj.val !== "")) {
+      setIsDisable(false);
+    } else {
+      setIsDisable(true);
+    }
+  }, [feildList]);
+
   return (
     <>
-      <h1>hjbjh</h1>
+      <h1>Form</h1>
+      <form onSubmit={handleSubmit}>
+        {feildList.map((field, i) => {
+          return (
+            <div key={field.id}>
+              <input
+                type="text"
+                name={`input${i + 1}`}
+                id={`input${i + 1}`}
+                value={feildList.val}
+                onChange={(e) => handleChange(i, e)}
+              />
+              <button type="button" onClick={() => handleRemove(i)}>
+                remove
+              </button>
+            </div>
+          );
+        })}
+        <br />
+        <button type="button" onClick={addFeild}>
+          Add
+        </button>
+        <button type="submit" disabled={isDisable}>
+          Submit
+        </button>
+      </form>
     </>
   );
 };
